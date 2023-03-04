5740c5ce76d53167f17e09356d8bb46fe0c1ee9c
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-03-05 00:34:54 +0300

    added primevue component library

---
+++ src/app/libs/primevue.config.js
@@ -0,0 +1,8 @@
+// здесь хранится экспорт всех нужных компонентов из библиотеки компонентов PrimeVue
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+
+export default [
+    {name: "InputText", component: InputText},
+    {name: "InputNumber", component: InputNumber},
+]
