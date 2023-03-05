b1c40d28a2198056c90beba50a2c7d82af06da8b
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-03-05 21:10:27 +0300

    added changest for user state

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,15 +2,21 @@ import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 
 export const useUserStore = defineStore('userStore', () => {
+  // states
   let user = reactive({
     firstName: null,
     lastName: null,
     middleName: null
   })
+  // -------------------------------------------------------------------
 
+  // mutations
   function addUserData({ field, value }) {
     user.value[field] = value
   }
+  // -------------------------------------------------------------------
+
+  // actions
 
   return { user, addUserData }
 })
