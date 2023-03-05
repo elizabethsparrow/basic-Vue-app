80ebd2399122f4d700c4e8622840b66f8d12a557
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-03-05 22:50:47 +0300

    добавлены перехватов запросов и ответов

--- src/app/libs/axios-instance.config.js
+++ src/app/libs/axios-instance.config.js
@@ -6,3 +6,25 @@ export const instance = axios.create({
     Authorization: localStorage.appToken
   }
 })
+
+// перехват работы запросов
+instance.interceptors.request.use(
+  (config) => {
+    // здесь можно сделать что-то перед отправкой запроса
+    return config
+  },
+  (error) => {
+    // здесь можно перехватить ошибку запроса
+    return Promise.reject(error)
+  }
+)
+
+// перехват ответов от сервера
+instance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
